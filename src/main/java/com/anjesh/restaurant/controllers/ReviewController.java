5e8df06db10264b73b4e0cb3c86354b3532fafa5
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-25 20:21:15 +0530

    Sorting for reviews implemented

--- src/main/java/com/anjesh/restaurant/controllers/ReviewController.java
+++ src/main/java/com/anjesh/restaurant/controllers/ReviewController.java
@@ -9,6 +9,10 @@ import com.anjesh.restaurant.mappers.ReviewMapper;
 import com.anjesh.restaurant.services.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -37,6 +41,21 @@ public class ReviewController {
         return ResponseEntity.ok(reviewMapper.toDto(createdReview));
     }
 
+    @GetMapping
+    public Page<ReviewDto> listReviews(
+            @PathVariable String restaurantId,
+            @PageableDefault(
+                    size = 20,
+                    page = 0,
+                    sort = "datePosted",
+                    direction = Sort.Direction.DESC)Pageable pageable
+            ){
+        return reviewService
+                .listReviews(restaurantId, pageable)
+                .map(reviewMapper::toDto);
+
+
+    }
     private User jwtToUser(Jwt jwt){
         return User.builder()
                 .id(jwt.getSubject())
--- src/main/java/com/anjesh/restaurant/services/ReviewService.java
+++ src/main/java/com/anjesh/restaurant/services/ReviewService.java
@@ -3,7 +3,11 @@ package com.anjesh.restaurant.services;
 import com.anjesh.restaurant.domain.ReviewCreateUpdateRequest;
 import com.anjesh.restaurant.domain.entities.Review;
 import com.anjesh.restaurant.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
     Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review);
+
+    Page<Review> listReviews(String restaurantId, Pageable pageable);
 }
